73154fc05eaa666b242050e6ca91763727048e6e
Steffano Pereira <sxp@a.recife.ifpe.edu.br>
2021-02-03 18:20:56 -0300

    Colisão de Personagem com objetos (#3)
    * feat: delimitação dos limites da hitbox do jogador
    
    - criação de um define para receber os valores da hitbox do jogador
    - X: Largura; Y: Altura do jogador
    
    * add: cenário de testes para colisão
    
    * fix: limites de acordo com o cenário
    
    - cenário agora é delimitado pelo objeto 1 de EnvItems
    
    * feat: condição de colisão com objetos
    
    * Fix: conserto do movimento do jogador na direita
    
    Refactor: mudança de variável em /limites na vertical/ pra do cenário
    
    * feat: colisão com objetos
    
    - adicionada a colisão com objetos a esquerda, direita e embaixo
    - método de teletransporte do jogador para a posição inicial
    
    * fix: bug de ficar colado no teto
    
    Co-authored-by: Koda-ms <julyanne.es@gmail.com>

--- main.c
+++ main.c
@@ -9,6 +9,14 @@
 #define JOGADOR_PULO_VELOCIDADE 500.0f
 #define JOGADOR_MOVIMENTO_VELOCIDADE 500.0f
 
+#define TAMANHO_X_JOGADOR 40
+#define TAMANHO_Y_JOGADOR 40
+
+#define TAMANHO_X_CENARIO 2000
+#define TAMANHO_Y_CENARIO 400
+
+bool colisaoJogador;
+
 typedef struct Jogador
 {
     Vector2 posicao;
@@ -42,11 +50,16 @@ int main()
 
     //Configurações Iniciais do Cenário
     EnvItem envItems[] = {
-        {{0, 0, 1000, 400}, 0, LIGHTGRAY},
-        {{0, 400, 1000, 200}, 1, GRAY},
+        {{0, 0, TAMANHO_X_CENARIO, TAMANHO_Y_CENARIO}, 0, SKYBLUE},
+        {{0, 400, 2000, 200}, 1, GRAY},
         {{300, 200, 400, 10}, 1, GRAY},
         {{250, 300, 100, 10}, 1, GRAY},
-        {{650, 300, 100, 10}, 1, GRAY}};
+        {{650, 300, 100, 10}, 1, GRAY},
+        {{900, 350,  50, 50}, 1, PURPLE},
+        {{1050, 311,  50, 50}, 1, PURPLE},
+        {{1200, 309,  50, 50}, 1, PURPLE},
+        {{1350, 330,  50, 50}, 1, PURPLE}
+    };
     int envItemsLength = sizeof(envItems) / sizeof(envItems[0]);
 
     //Configurações Iniciais de Câmera
@@ -80,8 +93,10 @@ int main()
         for (int i = 0; i < envItemsLength; i++)
             DrawRectangleRec(envItems[i].retangulo, envItems[i].cor);
 
-        Rectangle playerRect = {jogador.posicao.x - 20, jogador.posicao.y - 40, 40, 40}; //Desenho do jogador
+        Rectangle playerRect = {jogador.posicao.x - TAMANHO_X_JOGADOR / 2, jogador.posicao.y - TAMANHO_Y_JOGADOR, TAMANHO_X_JOGADOR, TAMANHO_Y_JOGADOR}; //Desenho do jogador
         DrawRectangleRec(playerRect, RED); //Desenha o desenho do jogador
+
+        DrawText(FormatText("Colisão : %01i", colisaoJogador), 1000, 450, 20, BLACK);
 
         EndMode2D();
 
@@ -98,10 +113,6 @@ int main()
 
 void UpdatePlayer(Jogador *jogador, EnvItem *envItems, int envItemsLength, float delta)
 {
-
-    //Limite da area do jogador na vertical
-    int limiteTelaSuperior = 40;
-
     if (IsKeyDown(KEY_LEFT)) //Movimentação para a Esquerda
         jogador->posicao.x -= JOGADOR_MOVIMENTO_VELOCIDADE * delta; //Decrementa o valor da posição do player
     if (IsKeyDown(KEY_RIGHT)) //Movimentação para a Direita
@@ -112,38 +123,66 @@ void UpdatePlayer(Jogador *jogador, EnvItem *envItems, int envItemsLength, float
         jogador->velocidade = -JOGADOR_PULO_VELOCIDADE;
         jogador->podePular = false;
     }
-
-    //Limites do jogador para a direita, esquerda e para cima
-    if ((jogador->posicao.x + 44) >= GetScreenWidth())
+    
+    //Limites da area de movimentação do jogador
+    if ((jogador->posicao.x + TAMANHO_X_JOGADOR / 2) > TAMANHO_X_CENARIO)
     {
-        jogador->posicao.x = GetScreenWidth() - 44; 
-    } else if (jogador->posicao.x <= 20)
+        jogador->posicao.x = TAMANHO_X_CENARIO - TAMANHO_X_JOGADOR / 2; //Limites para direita
+    } else if (jogador->posicao.x < TAMANHO_X_JOGADOR / 2)
     {
-        jogador->posicao.x = 20;
+        jogador->posicao.x = TAMANHO_X_JOGADOR / 2; //Limites para a esquerda
     }
     
-    if ((jogador->posicao.y + 200) >= GetScreenHeight())
+    if ((jogador->posicao.y) > TAMANHO_Y_CENARIO)  //Limites na vertical
     {
-        jogador->posicao.y = GetScreenHeight() - 200;
-    } else if (jogador->posicao.y <= limiteTelaSuperior)
+        jogador->posicao.y = TAMANHO_Y_CENARIO; 
+    } else if (jogador->posicao.y < TAMANHO_Y_JOGADOR)
     {
-        jogador->posicao.y = limiteTelaSuperior;
+        jogador->posicao.y = TAMANHO_Y_JOGADOR;
     }
 
+    colisaoJogador = 0;
     int colisaoObjeto = 0;
     for (int i = 0; i < envItemsLength; i++) //Preechimento da áre dos pixels dos objetos colidiveis
     {
         EnvItem *objeto = envItems + i;
         Vector2 *j = &(jogador->posicao);
+        
+        //Condição de colisão para pulo e andar encima de plataformas
         if (objeto->colisao &&                                             
-            objeto->retangulo.x <= j->x &&                                      //
-            objeto->retangulo.x + objeto->retangulo.width >= j->x &&            // Definindo a invasão da área do player com a área do objeto(área de colisão)   
+            objeto->retangulo.x - TAMANHO_X_JOGADOR/2 <= j->x &&                                      //
+            objeto->retangulo.x + objeto->retangulo.width + TAMANHO_X_JOGADOR/2 >= j->x &&            // Definindo a invasão da área do player com a área do objeto(área de colisão)   
             objeto->retangulo.y >= j->y &&                    
             objeto->retangulo.y < j->y + jogador->velocidade * delta)
         {
             colisaoObjeto = 1; 
             jogador->velocidade = 0.0f; //Reduzindo a velocidade do player para 0, para freiar ele             
             j->y = objeto->retangulo.y; //Atualiza a variável do movimento
+        }
+        
+        //Condição de colisão em objetos Universais
+        if (objeto->colisao && //Detecta se o objeto é colidível
+            objeto->retangulo.x - TAMANHO_X_JOGADOR/2 <= j->x && //Detecta a borda esquerda do objeto
+            objeto->retangulo.x + objeto->retangulo.width + TAMANHO_X_JOGADOR/2 >= j->x && //Detecta a borda direita do objeto
+            objeto->retangulo.y < j->y && //Detecta colisão acima do objeto
+            objeto->retangulo.y + objeto->retangulo.height + TAMANHO_Y_JOGADOR > j->y) //Detecta colisão abaixo do objeto
+        {
+            if (objeto->retangulo.x - TAMANHO_X_JOGADOR / 2 <= j->x &&
+                objeto->retangulo.x > j->x) //Detecta a colisão com a esquerda do objeto
+            {
+                jogador->posicao.x = objeto->retangulo.x - TAMANHO_X_JOGADOR / 2;
+            }
+            else if (objeto->retangulo.x + objeto->retangulo.width + TAMANHO_X_JOGADOR / 2 >= j->x &&
+                     objeto->retangulo.x + objeto->retangulo.width < j->x) //Detecta a colisão a direita do objeto
+            {
+                jogador->posicao.x = objeto->retangulo.x + objeto->retangulo.width + TAMANHO_X_JOGADOR / 2;
+            }
+            else if (objeto->retangulo.y + objeto->retangulo.height + TAMANHO_Y_JOGADOR > j->y) //Detecta a colisão abaixo do objeto
+            {
+                jogador->posicao.y = objeto->retangulo.y + objeto->retangulo.height + TAMANHO_Y_JOGADOR;
+                jogador->velocidade = GRAVIDADE * delta;
+            }
+            colisaoJogador = 1;
         }
     }
 
