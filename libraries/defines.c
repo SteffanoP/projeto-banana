094ed9be480da961d810c28d178d5631faa8ecdf
SteffanoP <steffanoxpereira@gmail.com>
2021-02-03 23:38:59 -0300

    style: arquivo para os defines de constantes
    Co-authored-by: Koda-ms <julyanne.es@gmail.com>

---
+++ libraries/defines.c
@@ -0,0 +1,14 @@
+#define screenWidth 1024
+#define screenHeight 768
+
+#define NOME_JOGO "O JOGO"
+
+#define GRAVIDADE 500
+#define JOGADOR_PULO_VELOCIDADE 500.0f
+#define JOGADOR_MOVIMENTO_VELOCIDADE 500.0f
+
+#define TAMANHO_X_JOGADOR 40
+#define TAMANHO_Y_JOGADOR 40
+
+#define TAMANHO_X_CENARIO 2000
+#define TAMANHO_Y_CENARIO 400
--- main.c
+++ main.c
@@ -1,19 +1,5 @@
 #include "raylib.h"
-
-#define screenWidth 1024 
-#define screenHeight 768 
-
-#define NOME_JOGO "O JOGO"
-
-#define GRAVIDADE 500
-#define JOGADOR_PULO_VELOCIDADE 500.0f
-#define JOGADOR_MOVIMENTO_VELOCIDADE 500.0f
-
-#define TAMANHO_X_JOGADOR 40
-#define TAMANHO_Y_JOGADOR 40
-
-#define TAMANHO_X_CENARIO 2000
-#define TAMANHO_Y_CENARIO 400
+#include "libraries/defines.c"
 
 bool colisaoJogador;
 
