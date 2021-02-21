9fdeb57a1b7d9d5f529bd0300293d4a593262b4a
Koda-ms <julyanne.es@gmail.com>
2021-02-20 22:34:07 -0300

    style: separar arquivo com structs e funções

--- main.c
+++ main.c
@@ -1,126 +1,16 @@
 #include "raylib.h"
 #include "libraries/defines.c"
+#include "libraries/main.h"
 #include "time.h"
 
 bool colisaoJogador;
-
-/* Sobre o jogador:
-posicao: Posição X e Y
-velocidade: velocidade de movimento do jogador
-podePular: condição em que pode pular
-vida: quantidade de vidas do jogador 
-direcao_movimento = player segue para a direita ou esquerda */
-
-typedef struct Jogador
-{
-    Vector2 tamanho;
-    Vector2 posicao;
-    Vector2 posicaoAnterior;
-    float velocidade;
-    bool podePular;
-    int vida;
-    int direcao_movimento;
-} Jogador;
-
-/* Sobre os inimigos:
-tipo: Tipo de inimigos
-tipo = 1 = minion
-tipo = 2 = gado
-tamanho: Tamanho do inimigo no cenário
-posicao: Posição do Inimigo no cenário
-velocidade: velocidade de movimentação
-direcao_movimento: direção em que se movimenta
-vida: quantidade de vidas do inimigo
-cor: Cor do inimigo*/
-typedef struct Inimigo
-{
-    int tipo;
-    Vector2 tamanho;
-    Vector2 posicao;
-    float velocidade;
-    bool direcao_movimento;
-    int vida;
-    Color cor;
-} Inimigo;
-
-/* Sobre os poderes:
-posicao: posicao em relação ao jogador
-velocidade: velocidade da movimentação
-raio: raio da forma (o poder representa um círculo)
-poder_ativo: verificação se está ativo ou não
-direcao_movimento: direção do poder independente da do jogador 
-cor: cor do poder */
-typedef struct Poder
-{
-    Vector2 posicao;
-    float raio;
-    bool poder_ativo;
-    bool direcao_movimento;
-    Color cor;
-} Poder;
-
-typedef struct EnvItem
-{
-    Rectangle retangulo;
-    int colisao;
-    Color cor;
-} EnvItem;
-
-typedef struct Personagem
-{
-    Vector2 posicao;
-    Texture2D texture;
-    float frameWidth;
-    float frameHeight;
-    Rectangle frameRect;
-} Personagem;
-
-typedef struct Minions
-{
-    Vector2 posicao;
-    Texture2D texture;
-    float frameWidth;
-    float frameHeight;
-    Rectangle frameRect;
-} Minions;
-
-typedef struct Gados
-{
-    Vector2 posicao;
-    Texture2D texture;
-    float frameWidth;
-    float frameHeight;
-    Rectangle frameRect;
-} Gados;
-
-typedef struct FPS_Animacao
-{
-    int counter;
-    float speed;
-    int currentFrame;
-} FPS_Animacao;
 
 int updateplayer;
 clock_t t;
 time_t s;
 time_t sc;
 
-static Poder imune_19[PODER_MAX_PERSONAGEM] = {0}; //A variável inicializa zerada em suas posições
-
-//Protótipo das funções
-
-void UpdatePlayer(Jogador *jogador, EnvItem *envItems, int envItemsLength, float delta);
-void UpdateInimigo(Inimigo *inimigo, EnvItem *envItems, Jogador *jogador, int tamanhoInimigos, int envItemsLength, float delta);
-void UpdatePoder(Poder *imune_19, Jogador *jogador, EnvItem *envItems, int envItemsLength, float delta);
-void AnimacaoJogadorMovimento(FPS_Animacao *frames, Jogador *jogador,Personagem *personagem, Inimigo *inimigo, Minions *minions, int tamanhoInimigos, float deltaTime);
-void AnimacaoInimigo(FPS_Animacao *frames, Inimigo *inimigo, Minions *minions, Gados *gados, int tamanhoInimigos, float deltaTime);
-void AnimacaoJogadorParado(Jogador *jogador, Personagem *personagem, float delta);
-void Draw(Camera2D camera, EnvItem *envItems, int envItemsLength, int tamanhoInimigo, Inimigo *inimigo, Minions *minions, Gados *gados, Jogador *jogador, Personagem *personagem);
-void UpdateCameraCenter(Camera2D *camera, Jogador *jogador, EnvItem *envItems, int envItemsLength, float delta, int width, int height);
-int VerificaColisaoBordasED(Vector2 entidade, float tamanho_entidade_x, float tamanho_entidade_y, Rectangle objeto);
-bool VerificaColisaoBordaS(Vector2 entidade, float tamanho_entidade_x, float tamanho_entidade_y, Rectangle objeto, int range);
-int VerificaRangeGado(Vector2 posicao_inicial, float tamanho_gado_x, float tamanho_gado_y, Rectangle jogador, float range);
-
+// Essa função fica no arquivo main.c*
 int main()
 {
     // Inicialização do Jogo
@@ -298,8 +188,7 @@ int main()
 
     return 0;
 }
-
-
+// Essa também*
 void UpdatePlayer(Jogador *jogador, EnvItem *envItems, int envItemsLength, float delta)
 {
     if (jogador->vida > 0)
@@ -385,7 +274,7 @@ void UpdatePlayer(Jogador *jogador, EnvItem *envItems, int envItemsLength, float
         jogador->podePular = true;
   
 }
-
+// Essa também*
 void UpdateInimigo(Inimigo *inimigo, EnvItem *envItems, Jogador *jogador, int tamanhoInimigos, int envItemsLength, float delta)
 {
     Rectangle ret_jogador = {jogador->posicao.x - (jogador->tamanho.x / 2),jogador->posicao.y - jogador->tamanho.y,jogador->tamanho.x,jogador->tamanho.y};
@@ -662,7 +551,7 @@ void AnimacaoJogadorParado(Jogador *jogador, Personagem *personagem, float delta
         jogador->velocidade += GRAVIDADE * delta; //Vai sofrer com a Gravidade
     }
 }
-
+// E essa*
 void Draw(Camera2D camera, EnvItem *envItems, int envItemsLength, int tamanhoInimigo, Inimigo *inimigo, Minions *minions, Gados *gados, Jogador *jogador, Personagem *personagem)
 {
     BeginDrawing();
