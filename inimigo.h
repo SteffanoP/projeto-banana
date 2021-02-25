bcaef065fc40439abcfb29981e2f49ed404a8df2
SteffanoP <steffanoxpereira@gmail.com>
2021-02-25 14:13:31 -0300

    refactor: direção de movimento dos inimigos
    *Adiciona direção de movimento dos inimigos na variável de testes de InimigoData

--- inimigo.h
+++ inimigo.h
@@ -33,47 +33,37 @@ int tamanhoInimigo = sizeof(inimigo) / sizeof(inimigo[0]);
 
 //Variáveis de Armazenamento de inimigo:
 InimigoData inimigoTeste[] = {
-    {1, {1650, 280}},
-    {1, {1750, 280}},
-    {1, {1850, 280}},
-    {2, {2050, 280}},
-    {2, {2150, 280}},
-    {3, {3310, 280}},
-    {3, {3210, 280}},
-    {3, {3110, 280}}
+    {1, {1650, 280}, 0},
+    {1, {1750, 280}, 0},
+    {1, {1850, 280}, 1},
+    {2, {2050, 280}, 1},
+    {2, {2150, 280}, 0},
+    {3, {3310, 280}, 0},
+    {3, {3210, 280}, 0},
+    {3, {3110, 280}, 0}
 };
 const int tamanho_inimigoTeste = sizeof(inimigoTeste) / sizeof(inimigoTeste[0]);
 
 //Dados dos inimigos do cenário 1
 InimigoData inimigoCenario1[] = {
-    {1, {1650, 280}},
-    {2, {1650, 280}}
+    
 };
 const int tamanho_inimigoCenario1 = sizeof(inimigoCenario1) / sizeof(inimigoCenario1[0]);
 
 //Dados dos inimigos do cenário 2
 InimigoData inimigoCenario2[] = {
-    {1, {1650, 280}},
-    {1, {1750, 280}},
-    {2, {2050, 280}},
-    {2, {2150, 280}}
+    
 };
 const int tamanho_inimigoCenario2 = sizeof(inimigoCenario2) / sizeof(inimigoCenario2[0]);
 
 //Dados dos inimigos do cenário 3
 InimigoData inimigoCenario3[] = {
-    {1, {1650, 280}},
-    {1, {1750, 280}},
-    {2, {2050, 280}},
-    {2, {2150, 280}}
+
 };
 const int tamanho_inimigoCenario3 = sizeof(inimigoCenario3) / sizeof(inimigoCenario3[0]);
 
 //Dados dos inimigos do cenário 4
 InimigoData inimigoCenario4[] = {
-    {1, {1650, 280}},
-    {1, {1750, 280}},
-    {2, {2050, 280}},
-    {2, {2150, 280}}
+    
 };
 const int tamanho_inimigoCenario4 = sizeof(inimigoCenario4) / sizeof(inimigoCenario4[0]);
