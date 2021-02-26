0ca6cb351c33d246262a5c890d9e6c69048fd9ae
Joaovcarvalho23 <joaovictorcordeiro00@gmail.com>
2021-02-26 00:48:17 -0300

    Funções Texture2D para cada bloco de cada cenário

---
+++ texturas_cenario.h
@@ -0,0 +1,216 @@
+//CENÁRIO_1//////////////////////////////////////////////////////////////////////
+/*
+Texture2D cenario_fundo10 = LoadTexture("blocos_cenario/leitecondensado.jpg.png");
+Texture2D cinza11 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D rosa12 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D marrom13 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D verde14 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D azul15 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D amarelo16 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D preto17 = LoadTexture("blocos_cenario/bloco_mario.png");
+Texture2D laranja18 = LoadTexture("blocos_cenario/Bandeira.png");
+
+
+DrawTextureRec(cenario_fundo10, cenario1[0].retangulo, (Vector2){0, 0}, RAYWHITE);
+DrawTextureRec(cinza11, cenario1[1].retangulo, (Vector2){0, 550}, RAYWHITE);
+DrawTextureRec(rosa12, cenario1[2].retangulo, (Vector2){560, 550}, RAYWHITE);
+DrawTextureRec(marrom13, cenario1[3].retangulo, (Vector2){1900, 550}, RAYWHITE);
+DrawTextureRec(verde14, cenario1[4].retangulo, (Vector2){850, 480}, RAYWHITE);
+DrawTextureRec(azul15, cenario1[5].retangulo, (Vector2){960, 410}, RAYWHITE);
+DrawTextureRec(amarelo16, cenario1[6].retangulo, (Vector2){1600, 650}, RAYWHITE);
+DrawTextureRec(preto17, cenario1[7].retangulo, (Vector2){960, 150}, RAYWHITE);
+DrawTextureRec(laranja18, cenario1[8].retangulo, (Vector2){2520, 350}, RAYWHITE);
+*/
+
+
+/*
+ //CENÁRIO_2//////////////////////////////////////////////////////////////////////
+
+DrawTextureRec(cenario_fundo20, cenario2[0].retangulo, (Vector2){0, 0}, RAYWHITE);
+DrawTextureRec(azul21, cenario2[1].retangulo, (Vector2){0, 550}, RAYWHITE);
+DrawTextureRec(amarelo22, cenario2[2].retangulo, (Vector2){1100, 550}, RAYWHITE);
+DrawTextureRec(marrom23, cenario2[3].retangulo, (Vector2){2300, 550}, RAYWHITE);
+DrawTextureRec(verde24, cenario2[4].retangulo, (Vector2){360, 450}, RAYWHITE);
+DrawTextureRec(vermelho25, cenario2[5].retangulo, (Vector2){650, 350}, RAYWHITE);
+DrawTextureRec(preto26, cenario2[6].retangulo, (Vector2){650, 70}, RAYWHITE);
+DrawTextureRec(rosa27, cenario2[7].retangulo, (Vector2){1520, 480}, RAYWHITE);
+DrawTextureRec(roxo28, cenario2[8].retangulo, (Vector2){1700, 360}, RAYWHITE);
+DrawTextureRec(cinza29, cenario2[9].retangulo, (Vector2){1920, 270}, RAYWHITE);
+DrawTextureRec(cinza210, cenario2[10].retangulo, (Vector2){2700, 480}, RAYWHITE);
+DrawTextureRec(cinza211, cenario2[11].retangulo, (Vector2){2770, 410}, RAYWHITE);
+DrawTextureRec(cinza212, cenario2[12].retangulo, (Vector2){2840, 340}, RAYWHITE);
+DrawTextureRec(cinza213, cenario2[13].retangulo, (Vector2){2910, 270}, RAYWHITE);
+DrawTextureRec(amarelo214, cenario2[14].retangulo, (Vector2){3250, 410}, RAYWHITE);
+DrawTextureRec(verde215, cenario2[15].retangulo, (Vector2){3500, 550}, RAYWHITE);
+DrawTextureRec(vermelho216, cenario2[16].retangulo, (Vector2){3750, 440}, RAYWHITE);
+DrawTextureRec(preto217, cenario2[17].retangulo, (Vector2){3760, 210}, RAYWHITE);
+DrawTextureRec(roxo218, cenario2[18].retangulo, (Vector2){4100, 550}, RAYWHITE);
+DrawTextureRec(azul219, cenario2[19].retangulo, (Vector2){4300, 480}, RAYWHITE);
+DrawTextureRec(laranja220, cenario2[20].retangulo, (Vector2){6020, 350}, RAYWHITE);
+
+
+Texture2D cenario_fundo20 = LoadTexture("blocos_cenario/banana.jpg.png");
+Texture2D azul21 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D amarelo22 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D marrom23 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D verde24 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D vermelho25 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D preto26 = LoadTexture("blocos_cenario/bloco_mario.png");
+Texture2D rosa27 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D roxo28 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D cinza29 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D cinza210 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D cinza211 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D cinza212 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D cinza213 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D amarelo214 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D verde215 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D vermelho216 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D preto217 = LoadTexture("blocos_cenario/bloco_mario.png");
+Texture2D roxo218 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D azul219 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D laranja220 = LoadTexture("blocos_cenario/Bandeira.png");
+
+*/
+
+
+//CENÁRIO_3////////////////////////////////////////////////////////////////////////////////
+/*      
+
+DrawTextureRec(cenario_fundo30, cenario3[0].retangulo, (Vector2){0, 0}, RAYWHITE);   
+DrawTextureRec(cinza31, cenario3[1].retangulo, (Vector2){0, 550}, RAYWHITE);
+DrawTextureRec(marrom32, cenario3[2].retangulo, (Vector2){2100, 550}, RAYWHITE);
+DrawTextureRec(azul33, cenario3[3].retangulo, (Vector2){0, 470}, RAYWHITE);
+DrawTextureRec(verde34, cenario3[4].retangulo, (Vector2){1150, 430}, RAYWHITE);
+DrawTextureRec(vermelho35, cenario3[5].retangulo, (Vector2){800, 310}, RAYWHITE);
+DrawTextureRec(rosa36, cenario3[6].retangulo, (Vector2){890, 130}, RAYWHITE);
+DrawTextureRec(preto37, cenario3[7].retangulo, (Vector2){635, 220}, RAYWHITE);
+DrawTextureRec(roxo38, cenario3[8].retangulo, (Vector2){565, 220}, RAYWHITE);
+DrawTextureRec(roxo39, cenario3[9].retangulo, (Vector2){705, 220}, RAYWHITE);
+DrawTextureRec(amarelo310, cenario3[10].retangulo, (Vector2){2400, 480}, RAYWHITE);
+DrawTextureRec(amarelo311, cenario3[11].retangulo, (Vector2){2470, 410}, RAYWHITE);
+DrawTextureRec(amarelo312, cenario3[12].retangulo, (Vector2){2540, 410}, RAYWHITE);
+DrawTextureRec(amarelo313, cenario3[13].retangulo, (Vector2){2610, 480}, RAYWHITE);
+DrawTextureRec(cinza314, cenario3[14].retangulo, (Vector2){3250, 430}, RAYWHITE);
+DrawTextureRec(azul315, cenario3[15].retangulo, (Vector2){3250, 210}, RAYWHITE);
+DrawTextureRec(roxo316, cenario3[16].retangulo, (Vector2){3220, 2}, RAYWHITE);
+DrawTextureRec(preto317, cenario3[17].retangulo, (Vector2){3290, 2}, RAYWHITE);
+DrawTextureRec(branco318, cenario3[18].retangulo, (Vector2){3550, 310}, RAYWHITE);
+DrawTextureRec(rosa319, cenario3[19].retangulo, (Vector2){3900, 550}, RAYWHITE);
+DrawTextureRec(azul320, cenario3[20].retangulo, (Vector2){4000, 480}, RAYWHITE);
+DrawTextureRec(azul321, cenario3[21].retangulo, (Vector2){4070, 410}, RAYWHITE);
+DrawTextureRec(laranja322, cenario3[22].retangulo, (Vector2){5950, 350}, RAYWHITE);
+
+
+Texture2D cenario_fundo30 = LoadTexture("blocos_cenario/laranja (1).png");
+Texture2D cinza31 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D marrom32 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D azul33 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D verde34 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D vermelho35 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D rosa36 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D preto37 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D roxo38 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D roxo39 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D amarelo310 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D amarelo311 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D amarelo312 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D amarelo313 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D cinza314 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D azul315 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D roxo316 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D preto317 = LoadTexture("blocos_cenario/bloco_mario.png");
+Texture2D branco318 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D rosa319 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D azul320 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D azul321 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D laranja322 = LoadTexture("blocos_cenario/Bandeira.png");
+
+*/
+
+
+//CENÁRIO_4//////////////////////////////////////////////////////////////////////////////
+/*
+
+DrawTextureRec(cenario_fundo40, cenario4[0].retangulo, (Vector2){0, 0}, RAYWHITE);
+DrawTextureRec(verde41, cenario4[1].retangulo, (Vector2){0, 450}, RAYWHITE);
+DrawTextureRec(marrom42, cenario4[2].retangulo, (Vector2){1900, 450}, RAYWHITE);
+DrawTextureRec(azul43, cenario4[3].retangulo, (Vector2){0, 150}, RAYWHITE);
+DrawTextureRec(amarelo44, cenario4[4].retangulo, (Vector2){1300, 450}, RAYWHITE);
+DrawTextureRec(preto45, cenario4[5].retangulo, (Vector2){1310, 210}, RAYWHITE);
+DrawTextureRec(cinza46, cenario4[6].retangulo, (Vector2){1600, 550}, RAYWHITE);
+DrawTextureRec(vermelho47, cenario4[7].retangulo, (Vector2){4670, 350}, RAYWHITE);
+DrawTextureRec(roxo48, cenario4[8].retangulo, (Vector2){4650, 70}, RAYWHITE);
+DrawTextureRec(preto49, cenario4[9].retangulo, (Vector2){4720, 70}, RAYWHITE);
+DrawTextureRec(azul410, cenario4[10].retangulo, (Vector2){3950, 610}, RAYWHITE);
+DrawTextureRec(cinza411, cenario4[11].retangulo, (Vector2){4300, 540}, RAYWHITE);
+DrawTextureRec(verde412, cenario4[12].retangulo, (Vector2){4400, 470}, RAYWHITE); 
+DrawTextureRec(laranja413, cenario4[13].retangulo, (Vector2){5950, 350}, RAYWHITE);
+
+Texture2D cenario_fundo40 = LoadTexture("blocos_cenario/banco_bananalto.jpg.png");
+Texture2D verde41 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D marrom42 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D azul43 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D amarelo44 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D preto45 = LoadTexture("blocos_cenario/bloco_mario.png");
+Texture2D cinza46 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D vermelho47 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D roxo48 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D preto49 = LoadTexture("blocos_cenario/bloco_mario.png");
+Texture2D azul410 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D cinza411 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D verde412 = LoadTexture("blocos_cenario/chao_mario.png");
+Texture2D laranja413 = LoadTexture("blocos_cenario/bandeira.png");
+
+*/
+
+
+//CENÁRIO_5//////////////////////////////////////////////////////////////////////////////
+/*
+
+DrawTextureRec(cenario_fundo50, cenario5[0].retangulo, (Vector2){0, 0}, RAYWHITE);
+DrawTextureRec(azul51, cenario5[1].retangulo, (Vector2){0, 1350}, RAYWHITE);
+DrawTextureRec(rosa52, cenario5[2].retangulo, (Vector2){1360, 1350}, RAYWHITE);
+DrawTextureRec(vermelho53, cenario5[3].retangulo, (Vector2){845, 1250}, RAYWHITE);
+DrawTextureRec(amarelo54, cenario5[4].retangulo, (Vector2){845, 1010}, RAYWHITE);
+DrawTextureRec(cinza55, cenario5[5].retangulo, (Vector2){340, 1130}, RAYWHITE);
+DrawTextureRec(roxo56, cenario5[6].retangulo, (Vector2){1360, 1130}, RAYWHITE);
+DrawTextureRec(roxo57, cenario5[7].retangulo, (Vector2){1430, 1060}, RAYWHITE);
+DrawTextureRec(laranja58, cenario5[8].retangulo, (Vector2){1630, 940}, RAYWHITE);
+DrawTextureRec(marrom59, cenario5[9].retangulo, (Vector2){1870, 821}, RAYWHITE);
+DrawTextureRec(verde510, cenario5[10].retangulo, (Vector2){2180, 720}, RAYWHITE);
+DrawTextureRec(verde511, cenario5[11].retangulo, (Vector2){2540, 650}, RAYWHITE);
+DrawTextureRec(preto512, cenario5[12].retangulo, (Vector2){2720, 420}, RAYWHITE); 
+DrawTextureRec(branco513, cenario5[13].retangulo, (Vector2){0, 890}, RAYWHITE);
+DrawTextureRec(branco514, cenario5[14].retangulo, (Vector2){0, 820}, RAYWHITE);
+DrawTextureRec(branco515, cenario5[15].retangulo, (Vector2){0, 750}, RAYWHITE);
+DrawTextureRec(branco516, cenario5[16].retangulo, (Vector2){0, 680}, RAYWHITE);
+DrawTextureRec(branco517, cenario5[17].retangulo, (Vector2){0, 610}, RAYWHITE);
+DrawTextureRec(azul518, cenario5[18].retangulo, (Vector2){530, 490}, RAYWHITE);
+DrawTextureRec(cinza519, cenario5[19].retangulo, (Vector2){730, 370}, RAYWHITE);
+DrawTextureRec(laranja520, cenario5[20].retangulo, (Vector2){2510, 170}, RAYWHITE);
+
+
+Texture2D cenario_fundo50 = LoadTexture("blocos_cenario/PALACIO_BANANALTO.png");
+Texture2D azul51 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D rosa52 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D vermelho53 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D amarelo54 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D cinza55 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D roxo56 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D roxo57 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D laranja58 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D marrom59 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D verde510 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D verde511 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D preto512 = LoadTexture("blocos_cenario/bloco_mario.png");
+Texture2D branco513 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D branco514 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D branco515 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D branco516 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D branco517 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D azul518 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D cinza519 = LoadTexture("blocos_cenario/chao_final.png");
+Texture2D laranja520 = LoadTexture("blocos_cenario/Bandeira.png");
+
+*/
